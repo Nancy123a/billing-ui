6b238c881b70a8ace25c591bd4283ec224dad5fb
Ali Shahbour <shahbour@gmail.com>
2017-11-04 15:32:51 +0200

    fix country code page

--- src/main/java/com/zeroandone/repository/OperatorCodeRepository.java
+++ src/main/java/com/zeroandone/repository/OperatorCodeRepository.java
@@ -3,11 +3,12 @@ package com.zeroandone.repository;
 import com.zeroandone.domain.OperatorCode;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface OperatorCodeRepository extends CrudRepository<OperatorCode,Integer> {
+public interface OperatorCodeRepository extends JpaRepository<OperatorCode,Integer> {
     @RestResource(path = "findByCountryName", rel = "findByCountryName")
     Page<OperatorCode> findByOperatorCountryCountryNameContainingIgnoreCase(@Param("countryName") String countryName, Pageable pageable);
 }
