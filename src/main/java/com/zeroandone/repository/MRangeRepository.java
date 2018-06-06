cfd3aab77fa37b9781d5922c4415f940fd72466a
nancy <n.boukamel@zeroandone.me>
2018-06-06 16:03:47 +0300

    1) Creating Range Database table 2) Creating DSL repository for range database table 3) Creating Projection for range database table 4) Creating view for range 5) Working on filling up range datatable 6) Add Range 7) Working on Select2 for Assignment and Carriers 8) Working on Searching range by From email 9) Validation of emails and form

--- src/main/java/com/zeroandone/repository/MRangeRepository.java
+++ src/main/java/com/zeroandone/repository/MRangeRepository.java
@@ -1,4 +1,17 @@
 package com.zeroandone.repository;
 
-public interface MRangeRepository {
+import com.querydsl.core.types.dsl.StringPath;
+import com.zeroandone.domain.MRange;
+import com.zeroandone.domain.QMRange;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+
+public interface MRangeRepository extends JpaRepository<MRange, Integer>,QueryDslPredicateExecutor<MRange>,QuerydslBinderCustomizer<QMRange> {
+
+    default void customize(QuerydslBindings bindings, QMRange qmRange)
+    {
+        bindings.bind(qmRange._From).first((StringPath path, String value) -> path.containsIgnoreCase(value));
+    }
 }
